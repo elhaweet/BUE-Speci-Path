46fff72ec0dd52904717e29b1ede7191b2571ebb
__H_X_H_X <amrelhaweet9@gmail.com>
2024-12-10 09:50:49 +0200

    Modules Weight adjusted

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const bodyParser = require("body-parser");
 const app = express();
 
 
-dotenv.config({ path: "./config/.env" });          // Check before running project
-// dotenv.config({ path: "./backend/config/.env" });
+// dotenv.config({ path: "./config/.env" });          // Check before running project
+dotenv.config({ path: "./backend/config/.env" });
 
 const port = process.env.PORT;
 
