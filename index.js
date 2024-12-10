4756314f61948cede69c333aeeb4f0c3c06d77a9
__H_X_H_X <amrelhaweet9@gmail.com>
2024-12-10 14:12:33 +0200

    Vercel 5

---
+++ index.js
@@ -0,0 +1,46 @@
+// Server.js
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const initDBConnection = require("./config/db");
+const recommendationRoutes = require("./routes/recommendationRoutes");
+
+const knowledgeHubRoutes = require("./routes/knowledgeHubRoutes");
+const careerRoutes = require("./routes/careerRoutes");
+const authRouter = require("./routes/auth");
+const reviewRoutes = require("./routes/reviewRoutes");
+
+const bodyParser = require("body-parser");
+const app = express();
+
+
+// dotenv.config({ path: "./config/.env" });          // Check before running project
+dotenv.config({ path: "./backend/config/.env" });
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+initDBConnection();
+
+
+// authRouter ---------------------------------------------------------------------------------------
+app.use("/", authRouter);
+
+// recommendationRoutes -----------------------------------------------------------------------------
+app.use("/", recommendationRoutes);
+
+// careerRoutes -------------------------------------------------------------------------------------
+app.use("/", careerRoutes);
+
+// knowledgeHubRoutes -------------------------------------------------------------------------------
+app.use("/", knowledgeHubRoutes);
+
+// reviewsRoutes -------------------------------------------------------------------------------------
+app.use('/', reviewRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
